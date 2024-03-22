5713a9dee59b2be430fad6e50c7a0aba3f03c95a
alberthamoui <albertdh@al.insper.edu.br>
2024-03-22 02:33:08 +0000

    ex3 3.1

--- exe3/main.c
+++ exe3/main.c
@@ -1,39 +1,46 @@
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
+#include <FreeRTOS.h>          
+#include <task.h>              
+#include <semphr.h>            
+#include <queue.h>             
 
-#include "pico/stdlib.h"
-#include <stdio.h>
 
-#include "data.h"
-QueueHandle_t xQueueData;
+#include "pico/stdlib.h"      
+#include <stdio.h>            
 
-// não mexer! Alimenta a fila com os dados do sinal
+#include "data.h"     
+QueueHandle_t xQueueData;   
+
+#define TAMANHO_JANELA 5
+
+int filtroMediaMovel(int newData, int dataBuffer[], int *indexPtr) {
+    static int sum = 0;
+    sum -= dataBuffer[*indexPtr]; // Tira o valor antigo
+    sum += newData; // Adiciona o valor novo
+    dataBuffer[*indexPtr] = newData; // Guarda a soma 
+    *indexPtr = (*indexPtr + 1) % TAMANHO_JANELA; // Atualiza o índice
+    return sum / TAMANHO_JANELA;
+}
+
 void data_task(void *p) {
     vTaskDelay(pdMS_TO_TICKS(400));
-
     int data_len = sizeof(sine_wave_four_cycles) / sizeof(sine_wave_four_cycles[0]);
     for (int i = 0; i < data_len; i++) {
         xQueueSend(xQueueData, &sine_wave_four_cycles[i], 1000000);
     }
-
     while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Aguarda um segundo
     }
 }
 
 void process_task(void *p) {
-    int data = 0;
+    int data_buffer[TAMANHO_JANELA] = {0}; // Tudo zero
+    int buffer_index = 0;
 
     while (true) {
-        if (xQueueReceive(xQueueData, &data, 100)) {
-            // implementar filtro aqui!
-
-
-
-
-            // deixar esse delay!
+        int data = 0;
+        if (xQueueReceive(xQueueData, &data, 100)) { // Tem dados na fila?
+            int filtered_data = filtroMediaMovel(data, data_buffer, &buffer_index);
+            printf("Filtrado: %d\n", filtered_data);
             vTaskDelay(pdMS_TO_TICKS(50));
         }
     }
@@ -41,14 +48,10 @@ void process_task(void *p) {
 
 int main() {
     stdio_init_all();
-
     xQueueData = xQueueCreate(64, sizeof(int));
-
     xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
     xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
-
     vTaskStartScheduler();
-
     while (true)
         ;
 }
